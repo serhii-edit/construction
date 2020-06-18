0dc20c82001643c3ffb8514bd8eb430c98da4327
Serhii Yakymenko <m.serhiiyakymenko@gmail.com>
2020-06-17 22:04:53 -0400

    Added slider and section offering

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
